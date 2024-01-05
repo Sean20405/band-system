bbde70a4fc1ea74c88bb7292eddaf949eab5fbef
ChuEating <chueating1005.cs11@nycu.edu.tw>
2024-01-05 21:51:39 +0800

    cookie

--- backend/app.py
+++ backend/app.py
@@ -54,7 +54,7 @@ def getcookie():
     name = request.cookies.get('userID')
     check = False
     print(name)
-    if (name is not "None"):
+    if (name is not None):
        check = True
 
     resp =  jsonify({
