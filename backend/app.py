d6fa235934b4148feb6291d0f72747d6990f8262
ChuEating <chueating1005.cs11@nycu.edu.tw>
2024-01-05 18:58:55 +0800

    POST cors

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 db.init_app(app)
 
