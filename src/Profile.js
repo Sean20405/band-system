f03b35e9c9d57c9d87898aa1994b5a497916b9e5
unknown <leekent173@gmail.com>
2024-01-06 15:49:03 +0800

    for vito

--- src/Profile.js
+++ src/Profile.js
@@ -1,3 +1,8 @@
+import { useRef , useState,useEffect} from 'react';
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+
+
 const Profile = ({user}) => {
     return  <h1>Welcome, {user.role} {user.user}!</h1>; 
 }
