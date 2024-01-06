0f0355eb7f2abbc1beee7a057738a2cfac76b7be
vitolin0416 <vitolin0416@gmail.com>
2024-01-06 15:33:54 +0800

    commit!

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,103 @@
+import { Link } from "react-router-dom";
+import useFetch from "./useFetch";
+import { useState, useEffect } from "react";
+
 const Profile = ({user}) => {
-    return  <h1>Welcome, {user.user}!</h1>; 
+    const id = user.user;
+    const role = user.role;
+    const [info, setInfo] = useState(null);
+    const [errMsg, setErrMsg] = useState('1234');
+    const [name, setName] = useState(null);
+    const [prefered_time, setPrefered_time] = useState(null);
+    const [instrument, setInstrument] = useState(null);
+    const [style, setStyle] = useState(null);
+    const [region, setRegion] = useState(null);
+    const [ig, setIg] = useState(null);
+    const [fb, setFb] = useState(null);
+    const [photo, setPhoto] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [bio, setBio] = useState(null);
+
+    let formData = new FormData(); 
+    formData.append('name', "Sheng_Shun_Chang");
+    formData.append('prefered_time', "midnight");
+    formData.append('bio', "love Amazing Show");
+    formData.append('photo', "Some random URL");
+    formData.append('ig', "vitolin_yucheng");
+    formData.append('fb', 'Kent_l');
+    formData.append('email', 'vitolin0416@gmail.com');
+    formData.append('instrument', 'violin');
+    formData.append('region', 'Taipei');
+    formData.append('style', 'rock paper scissor');
+
+    useEffect(()=>{
+        console.log("hi");
+        if(info){
+                  
+        }
+        else {
+            console.log(info);
+            console.log("cannot fetch info");
+        }
+    },[info])
+
+    useEffect(() =>{
+        console.log("init");
+        fetch('http://100.26.49.80:5000/user?user_id=' + id, {
+        method: 'GET',
+        headers:{
+            "ngrok-skip-browser-warning": "69420",
+        },
+        }).then((response) => {
+            return response.json(); 
+        }).then((data) => {
+            console.log(data);
+            setInfo(data);
+            console.log(info);
+            // setRegion(data.region);
+            // setName(data.name);
+            // setPrefered_time(data.prefered_time);
+            // setInstrument(data.instrument);
+            // setStyle(data.style);
+        })
+        .catch((error) => {
+            console.log(`Error: ${error.message}`);
+        })
+    },[])
+
+    
+
+    return(
+        <div className="profile">
+            <h1>{ formData.get("name") }'s Public profile</h1>
+            <br></br>
+            <div className="blog-preview" >
+                <h2>ID:</h2>
+                <p>{ id }</p><br></br>
+                <h2>Name:</h2>
+                <p>{ info.name }</p><br></br>
+                <h2>Prefered_time:</h2>
+                <p>{ info.prefered_time }</p><br></br>
+                <h2>Instument: </h2>
+                <p>{ info.instrument }</p> <br></br>
+                <h2>Style: </h2>
+                <p>{ info.style }</p> <br></br>
+                <h2>Region: </h2> 
+                <p>{ info.region }</p> <br></br>
+                <h2>Instagram: </h2> 
+                <p>@{ formData.get("ig")}</p> <br></br>
+                <h2>Facebook: </h2> 
+                <p>{ formData.get("fb")}</p> <br></br>
+                <h2>Email: </h2> 
+                <p>{ formData.get("email")}</p> <br></br>
+                <h2>Profile Picture: </h2> 
+                <p>{ formData.get("photo")}</p> <br></br>
+                <h2>Bio: </h2> 
+                <p>{ formData.get("bio")}</p> <br></br>
+            </div>
+        </div>
+
+    ); 
 }
  
 export default Profile;
